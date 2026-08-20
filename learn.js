23c72759a9180dd9e342647302bccde92f2ec8bc
razawarraich <muhammadraza5911g@gmail.com>
2026-08-20 23:44:31 +0500

    Add variables practice

---
+++ learn.js
@@ -0,0 +1,24 @@
+//console.log("hello");
+//console.log("hello2");
+
+//let age=23;
+//age=34;
+//console.log(age)
+//var age2=233;
+//console.log("HELLO")
+//const age3=21;
+//console.log([age, age2, age3]);
+//console.table([age, age2, age3]);
+let number=22;
+console.log(typeof number);
+let NumberToString=String(number)
+console.log(typeof NumberToString);
+let string="warraich";
+console.log(string);
+
+let StringToNumber= Number(string)
+console.log(StringToNumber);
+let isloggedin= "warraichj"
+let ConvertBooleanToNo= Number(isloggedin)
+console.log(typeof ConvertBooleanToNo);
+
