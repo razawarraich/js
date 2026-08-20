8016eb04546706469e361589f9af3e54cb781f17
razawarraich <muhammadraza5911g@gmail.com>
2026-08-21 00:12:24 +0500

    Practice comparison operators

--- learn.js
+++ learn.js
@@ -1,14 +1,14 @@
-//console.log("hello");
-//console.log("hello2");
+console.log("hello");
+console.log("hello2");
 
-//let age=23;
-//age=34;
-//console.log(age)
-//var age2=233;
-//console.log("HELLO")
-//const age3=21;
-//console.log([age, age2, age3]);
-//console.table([age, age2, age3]);
+let age=23;
+age=34;
+console.log(age)
+var age2=233;
+console.log("HELLO")
+const age3=21;
+console.log([age, age2, age3]);
+console.table([age, age2, age3]);
 let number=22;
 console.log(typeof number);
 let NumberToString=String(number)
