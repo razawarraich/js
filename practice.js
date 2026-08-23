f32945ad2b606977e12ad6b5ec8dd52bc5871f08
razawarraich <muhammadraza5911g@gmail.com>
2026-08-23 15:01:14 +0500

    practice

--- practice.js
+++ practice.js
@@ -1,20 +1,55 @@
-// let value = 21;
-// console.log(value)
-// const value2 = 22;
-// console.log(value2);
-// let StringValue="raza"
-// let Numbervalue= 32
-// let UserNull = null
-// let usernull;
-// console.log(typeof StringValue)
-// console.log(typeof Numbervalue);
-// console.log(typeof UserNull);
-// console.log(typeof usernull);
+let value = 21;
+console.log(value)
+const value2 = 22;
+console.log(value2);
+let StringValue="raza"
+let Numbervalue= 32
+let UserNull = null
+let usernull;
+console.log(typeof StringValue)
+console.log(typeof Numbervalue);
+console.log(typeof UserNull);
+console.log(typeof usernull);
 let Stringvalue = String("this is string")
 let numbers = Number(54)
 let symbledatatype = Symbol("id")
 let sybole2 = Symbol("id")
 console.log(symbledatatype == sybole2)
+
+let person ={
+    Name : "raza",
+    age:32,
+    City : "lahore"
+
+}
+
+
+let People = ["ali", "saad", "saud"]
+
+function hello(){
+    console.log("Hello World");
+    
+}
+
+hello ()
+
+let value = "250"
+value = Number(value)
+console.log(typeof value);
+
+
+let newstring= "250abc"
+newstring= Number(newstring)
+console.log(typeof newstring);
+console.log(newstring);
+console.log(parseInt(newstring) );
+console.log(typeof NaN);
+
+
+
+
+
+
 
 
 
