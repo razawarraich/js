9946fcf65afc349472887091fa9b9607fa4f7c31
razawarraich <muhammadraza5911g@gmail.com>
2026-08-23 14:29:58 +0500

    practice

---
+++ practice.js
@@ -0,0 +1,22 @@
+// let value = 21;
+// console.log(value)
+// const value2 = 22;
+// console.log(value2);
+// let StringValue="raza"
+// let Numbervalue= 32
+// let UserNull = null
+// let usernull;
+// console.log(typeof StringValue)
+// console.log(typeof Numbervalue);
+// console.log(typeof UserNull);
+// console.log(typeof usernull);
+let Stringvalue = String("this is string")
+let numbers = Number(54)
+let symbledatatype = Symbol("id")
+let sybole2 = Symbol("id")
+console.log(symbledatatype == sybole2)
+
+
+
+
+
