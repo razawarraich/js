2c3ba428328eacbc33897ed4b3fd09a286168e02
razawarraich <muhammadraza5911g@gmail.com>
2026-08-23 15:28:22 +0500

    practice

--- practice.js
+++ practice.js
@@ -1,49 +1,90 @@
-let value = 21;
-console.log(value)
-const value2 = 22;
-console.log(value2);
-let StringValue="raza"
-let Numbervalue= 32
-let UserNull = null
-let usernull;
-console.log(typeof StringValue)
-console.log(typeof Numbervalue);
-console.log(typeof UserNull);
-console.log(typeof usernull);
-let Stringvalue = String("this is string")
-let numbers = Number(54)
-let symbledatatype = Symbol("id")
-let sybole2 = Symbol("id")
-console.log(symbledatatype == sybole2)
+// let value = 21;
+// console.log(value)
+// const value2 = 22;
+// console.log(value2);
+// let StringValue="raza"
+// let Numbervalue= 32
+// let UserNull = null
+// let usernull;
+// console.log(typeof StringValue)
+// console.log(typeof Numbervalue);
+// console.log(typeof UserNull);
+// console.log(typeof usernull);
+// let Stringvalue = String("this is string")
+// let numbers = Number(54)
+// let symbledatatype = Symbol("id")
+// let sybole2 = Symbol("id")
+// console.log(symbledatatype == sybole2)
 
-let person ={
-    Name : "raza",
-    age:32,
-    City : "lahore"
+// let person ={
+//     Name : "raza",
+//     age:32,
+//     City : "lahore"
 
-}
+// }
 
 
-let People = ["ali", "saad", "saud"]
+// let People = ["ali", "saad", "saud"]
 
-function hello(){
-    console.log("Hello World");
+// function hello(){
+//     console.log("Hello World");
     
-}
+// }
 
-hello ()
+// hello ()
 
-let value = "250"
+// let value = "250"
+// value = Number(value)
+// console.log(typeof value);
+
+
+// let newstring= "250abc"
+// newstring= Number(newstring)
+// console.log(typeof newstring);
+// console.log(newstring);
+// console.log(parseInt(newstring) );
+// console.log(typeof NaN);
+
+
+// let value= "helloworld"
+// value= Boolean(value)
+// console.log(typeof value, value)
+
+// let booleanvalue= false;
+// booleanvalue= Number(booleanvalue)
+// console.log(typeof booleanvalue, booleanvalue);
+
+
+// let value = NaN
+// value= Boolean(value)
+// console.log(typeof value, value);
+
+
+// let isloggedin= true
+// isloggedin= Number(isloggedin)
+// console.log(typeof isloggedin , isloggedin);
+
+// let user= null;
+// user = Number(user)
+// console.log(typeof user, user);
+
+
+// let user= null
+// user= Boolean(user)
+// console.log(typeof user, user);
+
+let value= ""
 value = Number(value)
-console.log(typeof value);
+console.log(typeof value, value);
+console.log(value);
 
 
-let newstring= "250abc"
-newstring= Number(newstring)
-console.log(typeof newstring);
-console.log(newstring);
-console.log(parseInt(newstring) );
-console.log(typeof NaN);
+
+
+
+
+
+
 
 
 
